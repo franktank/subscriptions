91c09c9e9516e921fa1c4ade2080892269563fba
Franky Liang <fliangz96@gmail.com>
2016-11-10 19:27:53 +0000

    Fix entry point for application

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import AddSubscriptionTable from '../containers/AddSubscriptionTable'
+import UpdateSubscriptionTable from '../containers/UpdateSubscriptionTable'
+
+const App = () => (
+  <div>
+    <AddSubscriptionTable />
+    <UpdateSubscriptionTable />
+  </div>
+)
+
+export default App
